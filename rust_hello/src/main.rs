40907164aa1076428070cc277fe9ccf2ad3d8c6a
yanxu <243071373@qq.com>
2022-10-11 14:02:38 +0800

    a

--- rust_hello/src/main.rs
+++ rust_hello/src/main.rs
@@ -1,4 +1,15 @@
 
 fn main() {
     println!("Hello, world!");
+    greet_hello();
+}
+
+fn greet_hello(){
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好!";
+    let english = "world,hello!";
+    let regions = [southern_germany,chinese,english];
+    for region in regions.iter() {
+        println!("{}",region);
+    }
 }
